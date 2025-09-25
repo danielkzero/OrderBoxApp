3f01ae1b7c2c86aa91c89a479f5308ea2aeab308
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-25 14:43:26 -0300

    Adicionando model tipos de pedidos

---
+++ app/Models/TiposPedidos.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TiposPedidos extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tipos_pedidos';
+
+    protected $fillable = [
+        'nome', 'excluido', 'ultima_alteracao', 'empresa_id'
+    ];
+
+    public function empresa()
+    {
+        return $this->belongsTo(Empresas::class, 'id', 'empresa_id');
+    }
+}
