400c7e347693af65125a209e607b568174b6cb85
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-25 08:07:19 -0300

    Adicionando model cidades ibge

---
+++ app/Models/CidadesIbge.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CidadesIbge extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cidades_ibge';
+
+    protected $fillable = [
+        'uf_codigo', 'uf_nome', 'meso_codigo', 'meso_nome', 'micro_codigo', 'micro_nome',
+        'municipio_codigo', 'municipio_nome', 'pop_urbana', 'pop_urbana_central', 'pop_rural',
+        'area_km2', 'pop_km2', 'lat', 'lon', 'pop_total', 'svg', 'path'
+    ];
+}
