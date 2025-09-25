e3d898ed52a565353462f0bedf56f5e64c7fe5c9
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-25 14:28:43 -0300

    Adicionando model clientes endereços

---
+++ app/Models/ClientesEnderecos.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ClientesEnderecos extends Model
+{
+    use HasFactory;
+
+    protected $table = 'clientes_enderecos';
+
+    protected $fillable = [
+        'empresa_id', 'cliente_id', 'rua', 'complemento', 'bairro', 'cep', 'municipio_codigo', 'ultima_alteracao'
+    ];
+
+    public function empresa()
+    {
+        return $this->belongsTo(Empresas::class, 'id', 'empresa_id');
+    }
+
+    public function cliente()
+    {
+        return $this->belongsTo(Clientes::class, 'id', 'cliente_id');
+    }
+
+    public function ibge()
+    {
+        return $this->belongsTo(CidadesIbge::class, 'municipio_codigo', 'municipio_codigo');
+    }
+}
