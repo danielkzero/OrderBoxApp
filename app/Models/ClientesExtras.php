6404b6af4d1311a379196e7c38c6bb59cacbe639
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-25 14:29:07 -0300

    Adicionando model clientes extras

---
+++ app/Models/ClientesExtras.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ClientesExtras extends Model
+{
+    use HasFactory;
+
+    protected $table = 'clientes_extras';
+
+    protected $fillable = [
+        'empresa_id', 'cliente_id', 'campo_extra_id', 'nome', 'tipo', 'valor_texto', 'valor_data', 'valor_decimal', 'nome_arquivo', 'valor_arquivo'
+    ];
+
+    public function empresa()
+    {
+        return $this->belongsTo(Empresas::class, 'id', 'empresa_id');
+    }
+
+    public function cliente()
+    {
+        return $this->belongsTo(Clientes::class, 'id', 'cliente_id');
+    }
+}
