83b4c82bb87ed7121e0e4fd6ddb68ecb5a7724db
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-25 10:40:23 -0300

    Adicionando model icms st

---
+++ app/Models/Icms_st.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Icms_st extends Model
+{
+    use HasFactory;
+
+    protected $table = 'icms_st';
+
+    protected $fillable = [
+        'empresa_id', 'codigo_ncm', 'nome_excecao_fiscal', 'estado_destino', 'tipo_st', 'valor_mva', 'valor_pmc',
+        'imcs_credito', 'imcs_destino', 'preco_considerado_no_calculo', 'reducao_de_base', 'excluido', 'ultima_alteracao'
+    ];
+
+    public function empresa()
+    {
+        return $this->belongsTo(Empresas::class, 'id', 'empresa_id');
+    }
+}
