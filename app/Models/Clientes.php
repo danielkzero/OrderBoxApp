7519b9a111ecc5610319b9672d7942640d935900
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-25 10:39:42 -0300

    Adicionando model clientes

---
+++ app/Models/Clientes.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Clientes extends Model
+{
+    use HasFactory;
+
+    protected $table = 'clientes';
+
+    protected $fillable = [
+        'empresa_id', 'icms_st_id', 'tipo', 'razao_social', 'nome_fantasia', 'cnpj', 'inscricao_estadual', 'suframa',
+        'rua', 'numero', 'complemento', 'bairro', 'cep', 'municipio_codigo', 'bloqueado', 'motivo_bloqueio_id', 'observacao',
+        'ultima_alteracao', 'excluido'
+    ];
+
+    public function empresa()
+    {
+        return $this->belongsTo(Empresas::class, 'id', 'empresa_id');
+    }
+
+    public function icms_st()
+    {
+        return $this->belongsTo(Icms_st::class, 'id', 'icms_st_id');
+    }
+
+    public function motivo_bloqueio()
+    {
+        return $this->belongsTo(MotivosBloqueios::class, 'id', 'motivo_bloqueio_id');
+    }
+}
