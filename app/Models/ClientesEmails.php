4400693da2b65b6edb3eb941342662875e0a9665
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-25 14:27:30 -0300

    Adicionando model clientes emails

---
+++ app/Models/ClientesEmails.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ClientesEmails extends Model
+{
+    use HasFactory;
+
+    protected $table = 'clientes_emails';
+
+    protected $fillable = [
+        'empresa_id', 'cliente_id', 'email', 'tipo', 'ultima_alteracao'
+    ];
+
+    public function empresa()
+    {
+        return $this->belongsTo(Empresas::class, 'id', 'empresa_id');
+    }
+
+    public function cliente()
+    {
+        return $this->belongsTo(Clientes::class, 'id', 'cliente_id');
+    }
+}
