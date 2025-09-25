0ed8889e64d0190d33c6a22a372c47df76bb6797
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-25 10:42:24 -0300

    Adicionando model tabelas preços cidades

---
+++ app/Models/TabelasPrecosCidades.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TabelasPrecosCidades extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tabelas_precos_cidades';
+
+    protected $fillable = [
+        'empresa_id', 'tabela_preco_id', 'municipio_codigo'
+    ];
+
+    public function empresa()
+    {
+        return $this->belongsTo(Empresa::class, 'id', 'empresa_id');
+    }
+
+    public function tabela()
+    {
+        return $this->belongsTo(TabelasPrecos::class, 'id', 'tabela_preco_id');
+    }
+
+    public function municipio()
+    {
+        return $this->belongsTo(CidadesIbge::class, 'municipio_codigo', 'municipio_codigo');
+    }
+}
