2be66d748d747b89ac8bdd866ce4d6297be70b44
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-25 14:42:13 -0300

    Adicionando model formas de pagamentos

---
+++ app/Models/FormasPagamentos.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FormasPagamentos extends Model
+{
+    use HasFactory;
+
+    protected $table = 'formas_pagamentos';
+
+    protected $fillable = [
+        'empresa_id', 'nome', 'excluido', 'ultima_alteracao'
+    ];
+
+    public function empresa()
+    {
+        return $this->belongsTo(Empresas::class, 'id', 'empresa_id');
+    }
+}
