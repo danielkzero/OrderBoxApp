d076acf5d7c44ba09d68e3209f6399e72773e126
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-25 08:08:44 -0300

    Adicionando model produto imagens

---
+++ app/Models/ProdutoImagens.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProdutoImagens extends Model
+{
+    use HasFactory;
+
+    protected $table = 'produto_imagens';
+
+    protected $fillable = [
+        'empresa_id', 'produto_id', 'imagem_base64', 'ordem'
+    ];
+
+    public function produto()
+    {
+        return $this->belongsTo(Produtos::class, 'id', 'produto_id');
+    }
+
+    public function empresa()
+    {
+        return $this->belongsTo(Empresas::class, 'id', 'empresa_id');
+    }
+}
