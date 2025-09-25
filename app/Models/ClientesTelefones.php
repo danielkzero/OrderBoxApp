d6b5a6cd1c3c22dfcecb0345e421b026dadf3446
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-25 10:40:01 -0300

    Adicionando model clientes telefones

---
+++ app/Models/ClientesTelefones.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ClientesTelefones extends Model
+{
+    use HasFactory;
+
+    protected $table = 'clientes_telefones';
+
+    protected $fillable = [
+        'empresa_id', 'cliente_id', 'numero', 'tipo', 'ultima_alteracao'
+    ];
+
+    public function empresa()
+    {
+        return $this->belongsTo(Empresas::class, 'id', 'empresa_id');
+    }
+
+    public function cliente()
+    {
+        return $this->belongsTo(Clientes::class, 'id', 'cliente_id');
+    }
+}
