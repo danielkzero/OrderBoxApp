feab0267bc91e75f0cd404f73cfe0962d005e2f5
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-25 14:37:59 -0300

    Adicionando model condições de pagamento

---
+++ app/Models/CondicoesPagamentos.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CondicoesPagamentos extends Model
+{
+    use HasFactory;
+
+    protected $table = 'condicoes_pagamentos';
+
+    protected $fillable = [
+        'empresa_id', 'nome', 'valor_minimo', 'excluido', 'ultima_alteracao'
+    ];
+
+    public function empresa()
+    {
+        return $this->belongsTo(Empresas::class, 'id', 'empresa_id');
+    }
+}
