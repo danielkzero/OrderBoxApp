fd793fbd860abd4128bf834dc4e95d9c362c29f8
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-25 08:06:45 -0300

    Adicionando controller produtos

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -2,13 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
 use Illuminate\Http\Request;
-use App\Model\Produtos;
 
-class ProdutosController extends Controller
+class ProdutoController extends Controller
 {
-    public function index() 
+    public function index()
     {
-        
+        $produtos = Produto::with(['categoria', 'precos', 'grades.variacoes'])->get();
+        return response()->json($produtos);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'empresa_id' => 'required|exists:empresas,id',
+            'nome' => 'required|string|max:255',
+            'categoria_id' => 'nullable|exists:categorias,id',
+            'codigo' => 'nullable|string|max:255',
+            'preco_tabela' => 'nullable|numeric',
+            'preco_minimo' => 'nullable|numeric',
+        ]);
+
+        $produto = Produto::create($request->all());
+        return response()->json($produto, 201);
+    }
+
+    public function show($id)
+    {
+        $produto = Produto::with(['categoria', 'precos', 'grades.variacoes'])->findOrFail($id);
+        return response()->json($produto);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $produto = Produto::findOrFail($id);
+        $produto->fill($request->all());
+        $produto->ultima_alteracao = now();
+        $produto->save();
+
+        return response()->json($produto);
+    }
+
+    public function destroy($id)
+    {
+        $produto = Produto::findOrFail($id);
+        $produto->delete();
+
+        return response()->json(['message' => 'Produto deletado com sucesso']);
     }
 }
