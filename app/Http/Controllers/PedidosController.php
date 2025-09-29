acca8f31eb54d7267624957016ce4238a8e55e96
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-29 09:39:39 -0300

    Adicionando na função index as empresas para testes de menu... (será revertido posteriormente e adicionando na rota correta para o layout menu)

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pedidos;
+use App\Models\Empresas;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -11,8 +12,10 @@ class PedidosController extends Controller
     public function index()
     {
         $pedidos = Pedidos::with(['itens'])->get();
+        $empresas = Empresas::get();
         return Inertia::render('Pedidos/Index', [
-            'pedidos' => $pedidos
+            'pedidos' => $pedidos,
+            'empresas' => $empresas
         ]);
     }
 }
