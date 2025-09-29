5d20795536e30dab369b4c1c6b1b4c48ef675df6
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-29 10:03:44 -0300

    Adicionando seeder para clientes

--- database/seeders/ClientesSeeder.php
+++ database/seeders/ClientesSeeder.php
@@ -13,39 +13,39 @@ class ClientesSeeder extends Seeder
      */
     public function run(): void
     {
-        Clientes::factory()
+        \App\Models\Clientes::factory()
             ->count(50) // 50 clientes
             ->create()
             ->each(function ($cliente) {
                 // telefones
                 $cliente->telefones()->createMany(
-                    \App\Models\ClienteTelefone::factory()->count(rand(1, 3))->make()->toArray()
+                    \App\Models\ClientesTelefones::factory()->count(rand(1, 3))->make()->toArray()
                 );
 
                 // emails
                 $cliente->emails()->createMany(
-                    \App\Models\ClienteEmail::factory()->count(rand(1, 2))->make()->toArray()
+                    \App\Models\ClientesEmails::factory()->count(rand(1, 2))->make()->toArray()
                 );
 
                 // endereços extras
                 $cliente->enderecos()->createMany(
-                    \App\Models\ClienteEndereco::factory()->count(rand(1, 2))->make()->toArray()
+                    \App\Models\ClientesEnderecos::factory()->count(rand(1, 2))->make()->toArray()
                 );
 
                 // campos extras
-                $cliente->extras()->createMany(
-                    \App\Models\ClienteExtra::factory()->count(rand(0, 2))->make()->toArray()
+                $cliente->campos_extras()->createMany(
+                    \App\Models\ClientesExtras::factory()->count(rand(0, 2))->make()->toArray()
                 );
 
                 // contatos
                 $cliente->contatos()->createMany(
-                    \App\Models\ClienteContato::factory()->count(rand(1, 3))->make()->toArray()
+                    \App\Models\ClientesContatos::factory()->count(rand(1, 3))->make()->toArray()
                 )->each(function ($contato) {
                     $contato->telefones()->createMany(
-                        \App\Models\ClienteContatoTelefone::factory()->count(rand(1, 2))->make()->toArray()
+                        \App\Models\ClientesContatosTelefones::factory()->count(rand(1, 2))->make()->toArray()
                     );
                     $contato->emails()->createMany(
-                        \App\Models\ClienteContatoEmail::factory()->count(rand(1, 2))->make()->toArray()
+                        \App\Models\ClientesContatosEmails::factory()->count(rand(1, 2))->make()->toArray()
                     );
                 });
             });
