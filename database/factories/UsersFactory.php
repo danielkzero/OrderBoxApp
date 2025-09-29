d1bc60dde0925854beb50de3e38f00410111828d
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-29 10:01:38 -0300

    Renomeando users factory

--- database/factories/UsersFactory.php
+++ database/factories/UsersFactory.php
@@ -5,11 +5,12 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\Roles;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
+class UsersFactory extends Factory
 {
     /**
      * The current password being used by the factory.
@@ -26,9 +27,12 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'telefone' => $this->faker->phoneNumber(),
+            'role_id' => Roles::inRandomOrder()->first()->id ?? Roles::factory()->create()->id,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'ultima_alteracao' => now()
         ];
     }
 
