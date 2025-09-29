4abfb8693e5b2f3b40c5f5a1d0e6438bf36359bc
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-29 09:40:50 -0300

    Alterando classes para não permitir timestamps.

--- app/Models/ClientesContatos.php
+++ app/Models/ClientesContatos.php
@@ -15,6 +15,8 @@ class ClientesContatos extends Model
         'empresa_id', 'cliente_id', 'nome', 'cargo', 'excluido'
     ];
 
+    public $timestamps = false;
+    
     public function empresa()
     {
         return $this->belongsTo(Empresas::class, 'id', 'empresa_id');
