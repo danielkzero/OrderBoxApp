47893d920ceef25d97f510d48f2d519b3457619a
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-29 10:05:55 -0300

    Adicionando seeder para Icms

---
+++ database/seeders/Icms_stSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Icms_st;
+
+class Icms_stSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Cria 20 regras de ICMS-ST distribuídas entre as empresas existentes
+        Icms_st::factory()->count(20)->create();
+    }
+}
