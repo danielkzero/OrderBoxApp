6d84580bf372a98e3043713add6decfcb1847c9f
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-29 10:00:41 -0300

    Adicionando factory para clientes telefones

--- database/factories/ClientesTelefonesFactory.php
+++ database/factories/ClientesTelefonesFactory.php
@@ -2,22 +2,23 @@
 
 namespace Database\Factories;
 
+use App\Models\ClientesTelefones;
+use App\Models\Empresas;
+use App\Models\Clientes;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ClientesTelefones>
- */
 class ClientesTelefonesFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = ClientesTelefones::class;
+
     public function definition(): array
     {
         return [
-            //
+            'empresa_id' => Empresas::inRandomOrder()->first()->id ?? Empresas::factory()->create()->id,
+            'cliente_id' => Clientes::inRandomOrder()->first()->id ?? Clientes::factory()->create()->id,
+            'numero' => $this->faker->phoneNumber(),
+            'tipo' => $this->faker->randomElement(['M', 'F', 'C']), // M=mobile, F=fixo, C=comercial (ajuste se tiver enum)
+            'ultima_alteracao' => now(),
         ];
     }
 }
