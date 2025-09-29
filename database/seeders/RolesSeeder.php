aa9f34c267ea048ef9564ed5828754a806f630b4
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-29 10:06:14 -0300

    Adicionando seeder para roles

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Roles;
+use Faker\Factory as Faker;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        Roles::factory()->create([
+            'empresa_id' => 1,
+            'nome' => 'Administrador',
+            'descricao' => $faker->sentence,
+            'permissoes' => [
+                'clientes' => 'crud',
+                'pedidos' => 'crud',
+            ],
+        ]);
+
+        Roles::factory()->count(20)->create();
+    }
+}
