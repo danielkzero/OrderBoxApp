3bd4f100164a975be17cb0580b44e4f24ca22d2d
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-29 09:59:46 -0300

    Adicionando factory para clientes enderecos

--- database/factories/ClientesEnderecosFactory.php
+++ database/factories/ClientesEnderecosFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Clientes;
+use App\Models\Empresas;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ClientesEnderecos>
@@ -17,7 +19,15 @@ class ClientesEnderecosFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'empresa_id' => Empresas::inRandomOrder()->first()->id ?? Empresas::factory()->create()->id,
+            'cliente_id' => Clientes::inRandomOrder()->first()->id ?? Clientes::factory()->create()->id,
+            'rua' => $this->faker->streetName,
+            'numero' => $this->faker->buildingNumber,
+            'complemento' => $this->faker->secondaryAddress,
+            'bairro' => $this->faker->citySuffix,
+            'cep' => $this->faker->postcode,
+            'municipio_codigo' => null, 
+            'ultima_alteracao' => now(),
         ];
     }
 }
