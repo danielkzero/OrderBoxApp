fef180a2a92d02634e33172e00668bdbbb492660
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-29 09:42:45 -0300

    Adicionando factory para clientes contatos

--- database/factories/ClientesContatosFactory.php
+++ database/factories/ClientesContatosFactory.php
@@ -3,7 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\Clientes;
+use App\Models\Empresas;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ClientesContatos>
  */
@@ -17,7 +18,10 @@ class ClientesContatosFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'empresa_id' => Empresas::inRandomOrder()->first()->id ?? Empresas::factory()->create()->id,
+            'cliente_id' => Clientes::inRandomOrder()->first()->id ?? Clientes::factory()->create()->id,
+            'nome' => $this->faker->name(),
+            'cargo' => $this->faker->jobTitle(),
         ];
     }
 }
