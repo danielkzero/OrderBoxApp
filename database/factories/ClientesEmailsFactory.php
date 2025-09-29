ec34b6eb79d153ea8751ad7ff3c4d253c9058010
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-29 09:43:29 -0300

    Adicionando factory para clientes emails

--- database/factories/ClientesEmailsFactory.php
+++ database/factories/ClientesEmailsFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Clientes;
+use App\Models\Empresas;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ClientesEmails>
@@ -17,7 +19,11 @@ class ClientesEmailsFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'empresa_id' => Empresas::inRandomOrder()->first()->id ?? Empresas::factory()->create()->id,
+            'cliente_id' => Clientes::inRandomOrder()->first()->id ?? Clientes::factory()->create()->id,
+            'email' => $this->faker->safeEmail(),
+            'tipo' => $this->faker->randomElement(['M', 'F', 'C']), // M=mobile, F=fixo, C=comercial (ajuste se tiver enum)
+            'ultima_alteracao' => now(),
         ];
     }
 }
