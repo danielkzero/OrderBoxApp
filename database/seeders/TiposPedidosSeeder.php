47eb2fc2ed92758a74050d6f3891fd87fbbaa239
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-30 09:13:58 -0300

    Adicionando seeder tipos de pedidos

---
+++ database/seeders/TiposPedidosSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TiposPedidosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\TiposPedidos::factory(3)->create();
+    }
+}
