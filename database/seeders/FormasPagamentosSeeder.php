d8d7c57d966099a5ad0f482afce71835d3bef3ce
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-30 09:13:07 -0300

    Adicionando seeder formas de pagamentos

---
+++ database/seeders/FormasPagamentosSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FormasPagamentosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\FormasPagamentos::factory()->count(20)->create();
+    }
+}
