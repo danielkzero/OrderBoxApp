32c0ddef94014211f19dd48971bb156ce81c2810
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-30 09:13:13 -0300

    Adicionando seeder pedidos

---
+++ database/seeders/PedidosSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PedidosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Pedidos::factory()
+            ->count(30) // cria 20 pedidos
+            ->create()
+            ->each(function ($pedido) {
+                // Itens do pedido
+                $pedido->itens()->createMany(
+                    \App\Models\PedidosItens::factory()
+                        ->count(rand(1, 5))
+                        ->make([
+                            'empresa_id' => $pedido->empresa_id,
+                        ])
+                        ->toArray()
+                );
+
+                // Extras do pedido
+                $pedido->extras()->createMany(
+                    \App\Models\PedidosExtras::factory()
+                        ->count(rand(0, 3))
+                        ->make([
+                            'empresa_id' => $pedido->empresa_id,
+                        ])
+                        ->toArray()
+                );
+            });
+    }
+}
