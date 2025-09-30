c5ba32e9f9291e8696c1cdec1bd6864675488424
Daniel S. Ramos <danikzero@hotmail.com>
2025-09-30 13:30:03 -0300

    Organizando corretamente a ordem das rotas

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers\AuthController;
 
-Route::middleware('auth')->group(function () {
-    Route::get('{empresa}/', function () {
-        return inertia('Pedidos/Index'); // página inicial logada
-    });
-
-    Route::resource('{empresa}/pedidos', App\Http\Controllers\PedidosController::class);
-    Route::resource('{empresa}/clientes', App\Http\Controllers\ClientesController::class);
-    Route::resource('{empresa}/produtos', App\Http\Controllers\ProdutosController::class);
+Route::get('/', function () {
+    return inertia('Welcome');
 });
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(function () {
+    Route::get('{empresa}/', function ($empresa) { return inertia('Pedidos/Index'); })->where('empresa', '[0-9]+');
+
+    Route::resource('{empresa}/pedidos', App\Http\Controllers\PedidosController::class)->where(['empresa' => '[0-9]+']);
+    Route::resource('{empresa}/clientes', App\Http\Controllers\ClientesController::class)->where(['empresa' => '[0-9]+']);
+    Route::resource('{empresa}/produtos', App\Http\Controllers\ProdutosController::class)->where(['empresa' => '[0-9]+']);
+});
