f695298ca0b8d86a8205cd959520d619171435ca
Daniel Ramos <danikzero@hotmail.com>
2025-09-29 22:16:31 -0300

    Adicionando url de empresa

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,13 @@ use Inertia\Inertia;
 use App\Http\Controllers\AuthController;
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
+    Route::get('{empresa}/', function () {
         return inertia('Pedidos/Index'); // página inicial logada
     });
 
-    Route::resource('pedidos', App\Http\Controllers\PedidosController::class);
-    Route::resource('clientes', App\Http\Controllers\ClientesController::class);
-    Route::resource('empresas', App\Http\Controllers\EmpresasController::class);
-    Route::resource('produtos', App\Http\Controllers\ProdutosController::class);
+    Route::resource('{empresa}/pedidos', App\Http\Controllers\PedidosController::class);
+    Route::resource('{empresa}/clientes', App\Http\Controllers\ClientesController::class);
+    Route::resource('{empresa}/produtos', App\Http\Controllers\ProdutosController::class);
 });
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
