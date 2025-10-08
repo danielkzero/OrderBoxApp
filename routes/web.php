92a6ed51772eaaad4baf52b6d6ef56de4aca626e
Daniel S. Ramos <danikzero@hotmail.com>
2025-10-08 15:30:42 -0300

    Importando classes, adicionando prefix, organizando e comentando código.

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,34 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PedidosController;
+use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\ProdutosController;
+use App\Http\Controllers\DashboardController;
 
+// Página inicial
 Route::get('/', function () {
     return inertia('Welcome');
 });
 
+// Autenticação
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::middleware('auth')->group(function () {
-    Route::get('{empresa}/', function ($empresa) { return inertia('Pedidos/Index'); })->where('empresa', '[0-9]+');
+// Rotas autenticadas
+Route::prefix('{empresa}')->middleware('auth')->group(function () {
 
-    Route::resource('{empresa}/pedidos', App\Http\Controllers\PedidosController::class)->where(['empresa' => '[0-9]+']);
-    Route::resource('{empresa}/clientes', App\Http\Controllers\ClientesController::class)->where(['empresa' => '[0-9]+']);
-    Route::resource('{empresa}/produtos', App\Http\Controllers\ProdutosController::class)->where(['empresa' => '[0-9]+']);
-    Route::resource('{empresa}/dashboard', App\Http\Controllers\DashboardController::class)->where(['empresa' => '[0-9]+']);
+    // Página inicial da empresa (lista de pedidos)
+    Route::get('', function ($empresa) {
+        return inertia('Pedidos/Index');
+    })->where('empresa', '[0-9]+');
+
+    // Recursos principais
+    Route::resource('pedidos', PedidosController::class)->where(['empresa' => '[0-9]+']);
+    Route::resource('clientes', ClientesController::class)->where(['empresa' => '[0-9]+']);
+    Route::resource('produtos', ProdutosController::class)->where(['empresa' => '[0-9]+']);
+    Route::resource('dashboard', DashboardController::class)->where(['empresa' => '[0-9]+']);
 });
