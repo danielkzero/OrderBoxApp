0fa553ee4be787498068be517599fc2146583534
Daniel S. Ramos <danikzero@hotmail.com>
2025-10-08 13:43:20 -0300

    Alterando script para ativação do menu

--- resources/js/components/SidebarLink.vue
+++ resources/js/components/SidebarLink.vue
@@ -1,16 +1,13 @@
 <template>
-  <Link
-    :href="href"
-    class="flex items-center space-x-3 p-2 rounded cursor-pointer"
-    :class="[
-      isActive
-        ? 'bg-indigo-600 text-indigo-50 dark:bg-indigo-600'
-        : 'hover:bg-indigo-600 hover:text-indigo-50 dark:hover:bg-indigo-600 hover:scale-105 transition-all'
-    ]">
-    <i :class="icon + ' text-xl'"></i>
-    <span v-if="!collapsed" class="text-sm font-medium truncate">
-      {{ label }}
-    </span>
+  <Link :href="href" class="flex items-center space-x-3 p-2 rounded cursor-pointer" :class="[
+    isActive
+      ? 'bg-indigo-600 text-indigo-50 dark:bg-indigo-600'
+      : 'hover:bg-indigo-600 hover:text-indigo-50 dark:hover:bg-indigo-600 hover:scale-105 transition-all'
+  ]">
+  <i :class="icon + ' text-xl'"></i>
+  <span v-if="!collapsed" class="text-sm font-medium truncate">
+    {{ label }}
+  </span>
   </Link>
 </template>
 
@@ -29,5 +26,8 @@ const props = defineProps({
 const page = usePage();
 
 // Verifica se a rota atual é igual ao href do menu
-const isActive = computed(() => page.url === props.href);
+const isActive = computed(() => {
+  const url = page.url.split('/').filter(Boolean);
+  return './' + url[1] === props.href
+});
 </script>
