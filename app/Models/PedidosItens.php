a771ea274b87828e724fe11a986dec55ab35879c
Daniel S. Ramos <danikzero@hotmail.com>
2026-02-23 11:59:38 -0300

    Adicionando model de itens dos pedidos

--- app/Models/PedidosItens.php
+++ app/Models/PedidosItens.php
@@ -13,12 +13,19 @@ class PedidosItens extends Model
 
     protected $fillable = [
         'empresa_id', 'pedido_id', 'produto_id', 'tabela_preco_id', 'preco_tabela', 'preco_liquido', 'ipi', 'st', 'subtotal', 'quantidade', 
-        'quantidade_grades','observacoes', 'descontos_do_vendedor', 'descontos_de_promocoes', 'descontos_de_cupom', 'escluido'
+        'quantidade_grades', 'observacoes', 'descontos_do_vendedor', 'descontos_de_promocoes', 'descontos_de_politicas', 'desconto_de_cupom', 'excluido'
+    ];
+
+    protected $casts = [
+        'descontos_do_vendedor' => 'array',
+        'descontos_de_promocoes' => 'array',
+        'descontos_de_politicas' => 'array',
+        'quantidade_grades' => 'array',
     ];
 
     public function empresa()
     {
-        return $this->belongsTo(Empresas::class, 'id', 'empresa_id');
+        return $this->belongsTo(Empresas::class, 'empresa_id', 'id');
     }
 
     public function pedido()
@@ -28,11 +35,11 @@ class PedidosItens extends Model
 
     public function produto()
     {
-        return $this->belongsTo(Produtos::class, 'id', 'produto_id');
+        return $this->belongsTo(Produtos::class, 'produto_id', 'id');
     }
 
     public function tabela_preco()
     {
-        return $this->belongsTo(TabelasPrecos::class, 'id', 'tabela_preco_id');
+        return $this->belongsTo(TabelasPrecos::class, 'tabela_preco_id', 'id');
     }
 }
